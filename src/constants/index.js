0155785cc67d0eabe291dc230584eff0b863a3a1
Martin-bot-man <martinwere14@gmail.com>
2025-11-15 23:08:50 +0300

    feat: added social links to my porfolio

--- src/constants/index.js
+++ src/constants/index.js
@@ -126,7 +126,7 @@ export const experiences = [
     {
         title: "Digital Lead",
         company_name: "i&M Bank",
-        icon: i&m.png,
+        icon: tesla,
         iconBg: "#fbc3bc",
         date: "Jan 2025 - todate",
         points: [
